4a5afd0862141d588c73a30a030519114733940d
Muhammad_Shahroz <bshahroz48@gmail.com>
2026-08-14 21:57:19 +0500

    components

--- src/app/dataformat-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DataformatPipe } from './dataformat-pipe';
-
-describe('DataformatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DataformatPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
