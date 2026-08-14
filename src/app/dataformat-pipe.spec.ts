cac54c97fe6cc24a614230ecdf0ad35f67948953
Muhammad_Shahroz <bshahroz48@gmail.com>
2026-08-14 21:29:04 +0500

    angular cli

---
+++ src/app/dataformat-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DataformatPipe } from './dataformat-pipe';
+
+describe('DataformatPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DataformatPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
