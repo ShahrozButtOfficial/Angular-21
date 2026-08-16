4941a1d4a85a0b4ffe2e5cfe6e641e448f7bc8a0
Muhammad_Shahroz <bshahroz48@gmail.com>
2026-08-16 19:51:39 +0500

    todo app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,16 @@
 import { Component, signal, computed ,effect} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { TodoApp } from './todo-app/todo-app';
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TodoApp],
   templateUrl: './app.html',
   styleUrl: './app.css'
   
 })
 export class App {
-  // name = signal('Sharoz');
-
-  user =signal<{name:string,age:number}>({name:'Sharoz',age:20});
-
-  updateName(value: string){
-    this.user.update((user)=>({...user,name:value}));
-  }
-
-  updateAge(){
-    this.user.update((user)=>({...user,age:user.age +1}));
-  }
+ 
 }
 
 
