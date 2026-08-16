0bfce5a51c897e66eb796542c9742cc754043150
Muhammad_Shahroz <bshahroz48@gmail.com>
2026-08-16 22:44:12 +0500

    signal template binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,43 @@
 import { Component, signal, computed ,effect} from '@angular/core';
-import { TodoApp } from './todo-app/todo-app';
-
 
 @Component({
   selector: 'app-root',
-  imports: [TodoApp],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
   
 })
 export class App {
- 
+//  count = signal(0);
+
+//  isLoggedIn = signal(false);
+
+
+//  items= signal<string[]>(['item1', 'item2', 'item3']);
+
+//  addItem(){
+//   this.items.update(items => [...items, `HTML`]);
+//  }
+
+  role = signal<'admin' | 'user'>('user');
+
+  features = signal<string[]>([
+    "Dashboard",
+    "Settings",
+    "Profile",
+  ]);
+
+  makeAdmin(){
+    this.role.set('admin');
+    this.features.update(feature=> [...feature,'Admin Panel','User Management']);
+  }
+
+  makeUser(){
+    this.role.set('user');
+    this.features.update(feature=> feature.filter(f => f !== 'Admin Panel' && f !== 'User Management'));
+  }
+
+
 }
 
 
