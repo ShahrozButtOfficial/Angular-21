6fa40c7056cf7ccd585ede0da407d6227ac6bd57
Muhammad_Shahroz <bshahroz48@gmail.com>
2026-08-16 18:29:00 +0500

    two way binding old

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,15 @@
 import { Component, signal, computed ,effect} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
   
 })
 export class App {
-  counter = signal<number>(2);
-  doubleCount = computed(()=> this.counter() * 2);
-
-  constructor() {
-    effect(() => {
-      console.log(`Double value: ${this.doubleCount()}`);
-    });
-  }
-
-  increment() {
-    this.counter.update((value) => value + 1);
-  }
+  username = 'Shahroz';
 }
 
 
