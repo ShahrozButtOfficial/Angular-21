672a83db55001c574fec0caee7fed59d5b08de92
Muhammad_Shahroz <bshahroz48@gmail.com>
2026-08-16 18:40:40 +0500

    two way bindig with objects

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,7 +9,17 @@ import { FormsModule } from '@angular/forms';
   
 })
 export class App {
-  name = signal('Sharoz');
+  // name = signal('Sharoz');
+
+  user =signal<{name:string,age:number}>({name:'Sharoz',age:20});
+
+  updateName(value: string){
+    this.user.update((user)=>({...user,name:value}));
+  }
+
+  updateAge(){
+    this.user.update((user)=>({...user,age:user.age +1}));
+  }
 }
 
 
