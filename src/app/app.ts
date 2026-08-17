655ba9c669daefbd900a1c986ec58d6edee59de6
Muhammad_Shahroz <bshahroz48@gmail.com>
2026-08-17 22:02:17 +0500

    reusable card comp

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,46 +1,15 @@
-import { CommonModule } from '@angular/common';
-import { Component, signal, computed, effect } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
+import { Component} from '@angular/core';
+import { Card } from './card/card';
 
-import { FormBuilder, ReactiveFormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [Card],
   templateUrl: './app.html',
   styleUrl: './app.css'
 
 })
 export class App {
-  // name = signal('');
-  // email = signal('');
-
-
-  // submitForm() {
-  //   console.log('Form submitted:', this.name(), this.email());
-  //}
-
-  user = signal({
-    name: '',
-    email: ''
-  });
-
-  form:any
-
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      name:[''],
-      email:['']
-    })
-
-
-    this.form.valueChanges.subscribe((value:any) => {this.user.set(value)})
-  }
-
-  submitForm() {
-    console.log('Form submitted:', this.user().name, this.user().email);
-  }
-
-
+  
 }
