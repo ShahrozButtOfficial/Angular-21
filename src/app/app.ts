bad52236315a6e3c1d5c32b58eff9f504738ba74
Muhammad_Shahroz <bshahroz48@gmail.com>
2026-08-17 12:59:29 +0500

    React Form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal, computed, effect } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
+
+import { FormBuilder, ReactiveFormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 
 })
 export class App {
-  name = signal('');
-  email = signal('');
+  // name = signal('');
+  // email = signal('');
 
+
+  // submitForm() {
+  //   console.log('Form submitted:', this.name(), this.email());
+  //}
+
+  user = signal({
+    name: '',
+    email: ''
+  });
+
+  form:any
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name:[''],
+      email:['']
+    })
+
+
+    this.form.valueChanges.subscribe((value:any) => {this.user.set(value)})
+  }
 
   submitForm() {
-    console.log('Form submitted:', this.name(), this.email());
+    console.log('Form submitted:', this.user().name, this.user().email);
   }
+
+
 }
