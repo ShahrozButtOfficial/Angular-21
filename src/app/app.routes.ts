cd0e3c239ff58692ff9226d0044fdb32d136b52f
Muhammad_Shahroz <bshahroz48@gmail.com>
2026-08-18 17:49:44 +0500

    Lifecycle Hooks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import { Products } from './products/products';
 
 
 
 export const routes: Routes = [
     
-     {path:'products', component: Products}
+    
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import {  RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet] ,
+  imports: [Child] ,
   templateUrl: './app.html',
   styleUrl: './app.css'
 
 })
 export class App {
- 
+  title = signal('Hello  From App Component');
+  show = signal(true);
+
+  changeTitle() {
+    this.title.set('Title Changed');
+  }
+
+  toggle() {
+    this.show.set(!this.show());
+  }
+
 }
