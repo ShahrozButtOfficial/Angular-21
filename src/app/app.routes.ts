3891d825ca96bd89722bc254c0eab7d5e0ae0718
Muhammad_Shahroz <bshahroz48@gmail.com>
2026-08-18 16:46:50 +0500

    qyery params

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { Productcomp } from './productcomp/productcomp'
+import { Products } from './products/products';
 
 
 
 export const routes: Routes = [
    {path: 'product/:id', component: Productcomp},
+   {path: 'products', component: Products},
+
      
 ];
