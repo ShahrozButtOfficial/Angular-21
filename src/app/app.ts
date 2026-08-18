bab756f7c71c233681893b0b770f9ac56f847755
Muhammad_Shahroz <bshahroz48@gmail.com>
2026-08-18 19:06:28 +0500

    api With signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,29 @@
 import { Component, inject, signal } from '@angular/core';
 import { UserService } from './user';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html'
 })
 export class App {
-  private userService = inject(UserService);
+  // private userService = inject(UserService);
 
-  users = signal<any[]>([]);
+  // users = signal<any[]>([]);
 
-  loadUsers() {
-    this.userService.getUsers().subscribe({
-      next: (data) => {
-        console.log('Data received:', data);
-        this.users.set(data);
-      },
-      error: (error) => {
-        console.error('Error:', error);
-      }
-    });
-  }
+  // loadUsers() {
+  //   this.userService.getUsers().subscribe({
+  //     next: (data) => {
+  //       console.log('Data received:', data);
+  //       this.users.set(data);
+  //     },
+  //     error: (error) => {
+  //       console.error('Error:', error);
+  //     }
+  //   });
+  // }
+
+
+    userservice = inject(UserService);
+    users: any = toSignal(this.userservice.getUsers(), { initialValue: [] }); 
 }
